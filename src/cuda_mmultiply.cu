6cbf863294aab3a8b796cb198ef993ae24918785
shaswot <shaswot@acsl.ics.keio.ac.jp>
2021-07-23 04:43:12 +0900

    added mmultiply source code

--- src/cuda_mmultiply.cu
+++ src/cuda_mmultiply.cu
@@ -1,92 +1,70 @@
-// This program computes matrix multiplication on the GPU using CUDA
-// By: Nick from CoffeeBeforeArch
-
-#include <cstdlib>
-#include <cassert>
+#include <istream>
 #include <iostream>
+#include <fstream>
 
-using namespace std;
+#include <stddef.h>
+#include <typeinfo>
+#include <stdexcept>
 
-__global__ void matrixMul(int *a, int *b, int *c, int N){
-    // Calculate the global row and column for each thread
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+// https://github.com/arpaka/mnist-loader
+// #include "../include/mnist_loader.h"
 
-    // Boundary check for our matrix
-    if(row < N && col < N){
-        // Accumulate a partial result
-        int tmp = 0;
-        for(int i = 0; i < N; i++){
-            tmp += a[row * N + i] * b[i * N + col];
-        }
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xview.hpp>
+#include <xtensor/xnpy.hpp>
 
-        // Write back the result
-        c[row * N + col] = tmp;
-    }
-}
-
-// Initializes a square matrix with random numbers between 0-100
-void init_matrix(int *m, int N){
-    for(int i = 0; i < N * N; i++){
-        m[i] = rand() % 100;
-    }
-}
-
-// Verify the result on the CPU
-void verify_result(int *a, int *b, int *c, int N){
-    int tmp;
-    // For every row...
-    for(int i = 0; i < N; i++){
-        // For every col...
-        for(int j = 0; j < N; j++){
-            // For every element in the row-col pair
-            tmp = 0;
-            for(int k = 0; k < N; k++){
-                tmp += a[i * N + k] * b[k * N + j];
-            }
-            
-            // Check each result
-            assert(tmp == c[i * N + j]);
-        }
-    }
-}
-
-int main(){
-    // Set our square matrix dimension (2^10 x 2^10 default) 
-    int N = 1 << 10;
-    size_t bytes = N * N * sizeof(int);
-
-    // Allocate memory for our matrices
-    int *a, *b, *c;
-    cudaMallocManaged(&a, bytes);
-    cudaMallocManaged(&b, bytes);
-    cudaMallocManaged(&c, bytes);
-
-    // Initialize our matrices
-    init_matrix(a, N);
-    init_matrix(b, N);
-
-    // Set our CTA and Grid dimensions
-    int threads = 16;
-    int blocks = (N + threads - 1) / threads;
-
-    // Setup our kernel launch parameters
-    dim3 THREADS(threads, threads);
-    dim3 BLOCKS(blocks, blocks);
-
-    // Launch our kernel
-    matrixMul<<<BLOCKS, THREADS>>>(a, b, c, N);
-    cudaDeviceSynchronize();
-
-    // Verify the result
-    verify_result(a, b, c, N);
-
-    cout << "PROGRAM COMPLETED SUCCESSFULLY!" << endl;
+template <class _Tp>
+xt::xarray<_Tp> matmul(xt::xarray<_Tp> a, 
+                       xt::xarray<_Tp> b) noexcept(false)
+{
+//     if (a.shape().size() != b.shape().size()) {
+//         throw std::runtime_error("Shape mismatching!");
+//     }
     
-    // Free allocated memory
-    cudaFree(a);
-    cudaFree(b);
-    cudaFree(c);
+    const unsigned int n = a.shape()[0]; // a rows
+    const unsigned int m = a.shape()[1]; // a cols
+    const unsigned int p = b.shape()[1]; // b cols
 
+//     std::cout<<"n = "<<n<<std::endl;
+//     std::cout<<"m = "<<m<<std::endl;
+//     std::cout<<"p = "<<p<<std::endl;
+    
+    xt::xarray<double>::shape_type shape = {n,p};
+    xt::xarray<float> out = xt::zeros<_Tp>(shape);
+
+//     std::cout<<out<<std::endl;
+
+    for (auto i = 0; i < n; ++i){
+        for (auto j = 0; j < p; ++j){
+            for (auto k = 0; k < m; ++k){
+                out(i,j) += a(i,k) * b(k,j);
+            }
+        }
+    }
+    return out;
+}
+
+
+int main()
+{
+    auto w_mat = xt::load_npy<float>("../data/random_array.npy");
+    auto i_vec = xt::load_npy<float>("../data/random_input.npy");
+    auto i_mat = xt::load_npy<float>("../data/random_input_mat.npy");
+
+    i_vec.reshape({-1, 1});
+    
+    auto y1 = matmul<float>(w_mat, i_vec);
+    auto y2 = matmul<float>(w_mat, i_mat);
+    std::cout << "Matrix" << std::endl << w_mat << std::endl;
+    std::cout << "Vector" << std::endl << i_vec << std::endl;
+    std::cout << "Matrix * Vector" << std::endl<< y1 <<std::endl;
+    std::cout<<"******************************" <<std::endl;
+    
+    std::cout << "Matrix A" << std::endl << w_mat << std::endl;
+    std::cout << "Matrix B" << std::endl << i_mat << std::endl;
+    std::cout << "Matrix * Matrix" << std::endl << y2 <<std::endl;
+
+    
     return 0;
 }
