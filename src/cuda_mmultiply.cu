d4701c75d31677f0c18f1ca61ece7101c96d3668
shaswot <shaswot@acsl.ics.keio.ac.jp>
2021-07-23 02:40:10 +0000

    erroneous GPU output

--- src/cuda_mmultiply.cu
+++ src/cuda_mmultiply.cu
@@ -37,6 +37,7 @@ void naive_matrix_multiply(T *A, T *B, T* C, int width, int C_rows, int C_cols)
     }
     // store result
     C[row * C_cols + col] = value;
+    
   }
   
 
@@ -81,30 +82,44 @@ int main(void)
     
   // load weights from npy files
     
+  //xt::xarray<float> dense_weights = xt::load_npy<float>("../data/dense_weights.npy");
+  // transpose weight matrix from (784,32) -> (32,784)
+  //xt::xarray<float> matrix_X = xt::transpose(dense_weights);
+  
+  // add extra dimension (784,) -> (784,1)
+  // xt::xarray<float> matrix_Y = xt::load_npy<float>("../data/image_65.npy");
+  // matrix_Y.reshape({-1,1});
+  
   xt::xarray<float> matrix_X = xt::load_npy<float>("../data/random_matrix.npy");
   xt::xarray<float> matrix_Y = xt::load_npy<float>("../data/random_input_mat.npy");
-  
+
   std::cout << "matrix_X SHAPE: " << xt::adapt(matrix_X.shape()) << std::endl;
   std::cout << "matrix_Y SHAPE: " << xt::adapt(matrix_Y.shape()) << std::endl;
   
-  int X_rows = matrix_X.shape()[0];
-  int X_cols = matrix_X.shape()[1];
+  unsigned long X_rows = matrix_X.shape()[0];
+  unsigned long X_cols = matrix_X.shape()[1];
   
-  int Y_rows = matrix_Y.shape()[0];
-  int Y_cols = matrix_Y.shape()[1];
+  unsigned long Y_rows = matrix_Y.shape()[0];
+  unsigned long Y_cols = matrix_Y.shape()[1];
   
-  int Z_rows = X_rows;
-  int Z_cols = Y_cols;
+  unsigned long Z_rows = X_rows;
+  unsigned long Z_cols = Y_cols;
   
-  int X_size = X_rows * X_cols;
-  int Y_size = Y_rows * Y_cols;
-  int Z_size = Z_rows * Z_cols;
+  unsigned long X_size = X_rows * X_cols;
+  unsigned long Y_size = Y_rows * Y_cols;
+  unsigned long Z_size = Z_rows * Z_cols;
   
   // host copies of X,Y,Z
   float *X = new float[X_size];
   float *Y = new float[Y_size]; 
   float *Z = new float[Z_size];
   float *Z_cpu = new float[Z_size];
+  
+  // auto data()const: Returns a constant pointer to the underlying array serving as element storage. 
+  // The pointer is such that range [data(); data() + size()] is always a valid range, even if the container is empty (data() is not is not dereferenceable in that case)
+  
+  X = matrix_X.data();
+  Y = matrix_Y.data();
   
   // device copies of X, Y, Z
   float *d_X, *d_Y, *d_Z;
@@ -119,7 +134,10 @@ int main(void)
   cudaMemcpy(d_Y, &Y, Y_size, cudaMemcpyHostToDevice);
   
   // Matrix Multiplication on GPU
-  dim3 dim_grid(Z_cols/COL_TILE_WIDTH, Z_rows/ROW_TILE_WIDTH, 1);
+  //dim3 dim_grid(Z_cols/COL_TILE_WIDTH, Z_rows/ROW_TILE_WIDTH, 1);
+  //dim3 dim_block(COL_TILE_WIDTH, ROW_TILE_WIDTH, 1);
+  
+  dim3 dim_grid(1, 1, 1);
   dim3 dim_block(COL_TILE_WIDTH, ROW_TILE_WIDTH, 1);
 
   naive_matrix_multiply<float><<<dim_grid, dim_block>>>(X, Y, Z, X_cols, Z_rows, Z_cols);
@@ -130,14 +148,25 @@ int main(void)
   // Copy result back to the host
   cudaMemcpy(&Z, d_Z, Z_size, cudaMemcpyDeviceToHost);
   
+  xt::xarray<double>::shape_type matrix_Z_shape = {Z_rows, Z_cols};
+  xt::xarray<float> matrix_Z = xt::adapt(Z, Z_size, xt::acquire_ownership(), matrix_Z_shape);
+  std::cout<<"GPU: matrix_Z"<<std::endl;
+  std::cout<<matrix_Z<<std::endl;
+  std::cout<<"**********************"<<std::endl;
+
   // Matrix Multiplication on CPU
   naive_matrix_multiply_cpu<float>(X, Y, Z_cpu, X_cols, Z_rows, Z_cols);
+  
+  xt::xarray<float> matrix_Z_cpu = xt::adapt(Z_cpu, Z_size, xt::acquire_ownership(), matrix_Z_shape);
+  std::cout<<"CPU: matrix_Z"<<std::endl;
+  std::cout<<matrix_Z_cpu<<std::endl;
   
   
   if(check_equal<float>(Z, Z_cpu, Z_rows, Z_cols))
     std::cout << "PASS" << std::endl;
   else
     std::cout << "FAIL" << std::endl;
+    
 
   // Free memory
   cudaFree(d_X);
