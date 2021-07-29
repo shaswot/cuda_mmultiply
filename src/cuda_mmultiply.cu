e5155cffc8673f82db965a922751629467ce888c
shaswot <shaswot@acsl.ics.keio.ac.jp>
2021-07-29 20:59:56 +0000

    cuda log is not written onto a file

--- src/cuda_mmultiply.cu
+++ src/cuda_mmultiply.cu
@@ -3,6 +3,7 @@
 #include <istream>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include <stddef.h>
 #include <typeinfo>
@@ -80,9 +81,9 @@ void naive_matrix_multiply(T *A, T *B, T* C, int width, int C_rows, int C_cols, 
 {
   int row = blockIdx.y * blockDim.y + threadIdx.y;   
   int col = blockIdx.x * blockDim.x + threadIdx.x;
-  uint bad_SM = 0;
-  uint bad_thrd = 23;
-  int error = 0.0f;
+  //uint bad_SM = 0;
+  //uint bad_thrd = 23;
+  //int error = 0.0f;
   
   // check boundry conditions
   if( row < C_rows && col < C_cols ){
@@ -106,7 +107,12 @@ void naive_matrix_multiply(T *A, T *B, T* C, int width, int C_rows, int C_cols, 
 
 template <class _Tp>
 xt::xarray<_Tp> matmul( xt::xarray<_Tp> matrix_X,
-                        xt::xarray<_Tp> matrix_Y)
+                        xt::xarray<_Tp> matrix_Y,
+                        gpc_id* myid)
+                        // array of bad SMs
+                        // array of bad threads
+                        // LAYER_WIDTH
+                        // Automatic BLOCK SIZE
 {
   unsigned long X_rows = matrix_X.shape()[0];
   unsigned long X_cols = matrix_X.shape()[1];
@@ -125,7 +131,7 @@ xt::xarray<_Tp> matmul( xt::xarray<_Tp> matrix_X,
   _Tp *X = new _Tp[X_size];
   _Tp *Y = new _Tp[Y_size]; 
   _Tp *Z = new _Tp[Z_size];
-  gpc_id *myid = new gpc_id[Z_size];
+  myid = new gpc_id[Z_size];
   
   // Allocate Unified Memory – accessible from CPU or GPU
   cudaMallocManaged(&X, X_size*sizeof(_Tp));
@@ -140,15 +146,14 @@ xt::xarray<_Tp> matmul( xt::xarray<_Tp> matrix_X,
    
   for (int i = 0; i < Y_size; i++)
   Y[i] = matrix_Y.flat(i);
-  
-  
+    
   // Matrix Multiplication on GPU
   // Determine Grid/Block Size
   // https://stackoverflow.com/questions/9985912/how-do-i-choose-grid-and-block-dimensions-for-cuda-kernels
   unsigned long ROW_TILE_WIDTH, COL_TILE_WIDTH;
   unsigned long DIM_COL_WIDTH, DIM_ROW_WIDTH;
   
-  ROW_TILE_WIDTH = 64; 
+  ROW_TILE_WIDTH = 64; //LAYER WIDTH
   COL_TILE_WIDTH = 1; 
 
   DIM_COL_WIDTH  = 1;
@@ -166,6 +171,40 @@ xt::xarray<_Tp> matmul( xt::xarray<_Tp> matrix_X,
   xt::xarray<double>::shape_type matrix_Z_shape = {Z_rows, Z_cols};
   xt::xarray<float> matrix_Z = xt::adapt(Z, Z_size, xt::no_ownership(), matrix_Z_shape);
   
+  
+  // Log the output of myid
+  // https://stackoverflow.com/questions/25918057/how-to-set-a-fixed-width-with-cout
+  size_t headerWidths[5] = {std::string("T_IDX  ").size(),
+                            std::string("WRP_ID  ").size(),
+                            std::string("SM_ID  ").size(),
+                            std::string("GRID_ID  ").size(),
+                            std::string("CTA_IDX  ").size()
+                            };
+  // Redirecting output to a file
+  // https://stackoverflow.com/questions/10150468/how-to-redirect-cin-and-cout-to-files
+  // https://stackoverflow.com/questions/29464578/append-std-output-of-a-function-to-a-file
+  std::ofstream out("cuda_log.txt", std::fstream::app);
+  std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+  std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
+
+  std::cout << "T_IDX  T_IDY  T_IDZ  WRP_ID  SM_ID  GRID_ID  CTA_IDX  CTA_IDY  CTA_IDZ"<< std::endl;
+  for (int i = 0; i < Z_size; i++){
+      std::cout << std::left << std::setw(headerWidths[0]) << myid[i].t_idx;
+      std::cout << std::left << std::setw(headerWidths[0]) << myid[i].t_idy;
+      std::cout << std::left << std::setw(headerWidths[0]) << myid[i].t_idz;
+      std::cout << std::left << std::setw(headerWidths[1]) << myid[i].warp_id;
+      std::cout << std::left << std::setw(headerWidths[2]) << myid[i].sm_id;
+      std::cout << std::left << std::setw(headerWidths[3]) << myid[i].grid_id;
+      std::cout << std::left << std::setw(headerWidths[4]) << myid[i].cta_idx;
+      std::cout << std::left << std::setw(headerWidths[4]) << myid[i].cta_idy;
+      std::cout << std::left << std::setw(headerWidths[4]) << myid[i].cta_idz;
+      std::cout << std::endl;
+  }
+  std::cout << "***************************" << std::endl;
+  
+  std::cout.rdbuf(coutbuf); //reset to standard output again
+  
+    
   // Free memory
   cudaFree(X);
   cudaFree(Y);
@@ -177,7 +216,7 @@ xt::xarray<_Tp> matmul( xt::xarray<_Tp> matrix_X,
 }
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
  // load weights from npy files
     xt::xarray<float> dense_weights = xt::load_npy<float>("../data/mnist_dense-w64-20210702_dense_weights.npy");
@@ -197,7 +236,13 @@ int main(void)
 
 
     /*check for the image <image_no> and display truth label*/
-    int image_no = 65;
+    // https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c
+    
+    gpc_id* myid_l1 = NULL;
+    gpc_id* myid_l2 = NULL;
+
+    
+    int image_no = std::stoi(argv[1]); //convert argument string to int
     int label = train.labels(image_no);
     std::cout << "IMAGE_NUMBER: " << image_no << std::endl;
     std::cout << "TRUTH_LABEL:  " << label << std::endl;
@@ -210,11 +255,15 @@ int main(void)
     input_image.reshape({-1, 1});
 
     /******************LAYER 1******************/
-    // transpose weight matrix from (784,32) -> (32,784)
+    // transpose weight matrix from (784, LAYER_WIDTH) -> (LAYER_WIDTH,784)
     xt::xarray<float> tr_dense_weights = xt::transpose(dense_weights);
     
     // send through layer
-    xt::xarray<float> l1 = matmul<float>(tr_dense_weights, input_image);
+    xt::xarray<float> l1 = matmul<float>(tr_dense_weights, input_image, myid_l1);
+    
+    //uint myid_l1_size;
+    //myid_l1_size = tr_dense_weights.shape()[0]*input_image.shape()[1];
+    //display_core_id_values(myid_l1, myid_l1_size);
     
     // first layer bias
     xt::xarray<float> b1 = l1 + dense_biases;
@@ -227,7 +276,7 @@ int main(void)
     xt::xarray<float> tr_dense_1_weights = xt::transpose(dense_1_weights);
     
     // send through layer
-    xt::xarray<float> l2 = matmul<float>(tr_dense_1_weights, b1_relu);
+    xt::xarray<float> l2 = matmul<float>(tr_dense_1_weights, b1_relu, myid_l2);
     
     // second layer bias
     xt::xarray<float> b2 = l2 + dense_1_biases;
