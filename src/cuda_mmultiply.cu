8a5e2f3fcb786861c3395f180ea83e490d598d60
shaswot <shaswot@acsl.ics.keio.ac.jp>
2021-08-27 13:58:30 +0000

    random mat-vec possible

--- src/cuda_mmultiply.cu
+++ src/cuda_mmultiply.cu
@@ -21,6 +21,11 @@
 #include <xtensor/xnpy.hpp>
 #include <xtensor/xsort.hpp>
 
+#include <curand.h>
+
+#define BLOCK_HEIGHT 1024
+#define BLOCK_WIDTH 64
+
 // GLOBAL VARIABLES
 uint LAYER_WIDTH = 128;
 uint MODEL_SEED = 20210702;
@@ -58,7 +63,8 @@ typedef struct GPC_ID gpc_id;
 
 // https://forums.developer.nvidia.com/t/any-way-to-know-on-which-sm-a-thread-is-running/19974/15
 // https://www.codeproject.com/Articles/15971/Using-Inline-Assembly-in-C-C
-__device__ gpc_id get_gpcid(void) {
+__device__ gpc_id get_gpcid(void) 
+{
      
      struct GPC_ID my_id;
      asm("mov.u32 %0, %tid.x;"    : "=r"(my_id.t_idx)    );
@@ -76,258 +82,263 @@ __device__ gpc_id get_gpcid(void) {
      return my_id;
 }
 
+void createRandomMatrix(float *A, int size, int seed) {
+  float *d_A;
+  float *h_A = (float *) malloc (size * sizeof(float));
+  curandGenerator_t gen;
+  size_t size_d_A = size * sizeof(float);
 
-// Matrix-vector multiplication using CUDA
-// Using shared memory and avoiding banking conflicts
-xt::xarray<_Tp> matmul( xt::xarray<_Tp> matrix_W,
-                        xt::xarray<_Tp> matrix_X)
-{
-    unsigned long X_rows = matrix_X.shape()[0];
-    unsigned long X_cols = matrix_X.shape()[1];
-  
-    unsigned long Y_rows = matrix_Y.shape()[0];
-    unsigned long Y_cols = matrix_Y.shape()[1];
-  
-    unsigned long Z_rows = X_rows;
-    unsigned long Z_cols = Y_cols;
-  
-    unsigned long X_size = X_rows * X_cols;
-    unsigned long Y_size = Y_rows * Y_cols;
-    unsigned long Z_size = Z_rows * Z_cols;
-  
-    // host copies of X,Y,Z
-    _Tp *X = new _Tp[X_size];
-    _Tp *Y = new _Tp[Y_size]; 
-    _Tp *Z = new _Tp[Z_size];
-    gpc_id *myid = new gpc_id[Z_size];
+  cudaMalloc((void **) &d_A, size_d_A);
+
+  curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+  curandSetPseudoRandomGeneratorSeed(gen, seed);
+
+  curandGenerateUniform(gen, d_A, size);
+
+  cudaMemcpy(h_A, d_A, size_d_A, cudaMemcpyDeviceToHost);
+
+  // for (int j = 0; j < 10; j++) 
+  //  printf("h_A[%d] = %l=f\n", j, 10* h_A[j]);
+
+  for (int j = 0; j < size; j++) 
+    A[j] = h_A[j] / sqrt (size); 
+
+  curandDestroyGenerator(gen);
+  cudaFree(d_A);
+  free(h_A);
 }
 
-// Matrix Multiplication using CUDA
-template<typename T>
-__global__
-void naive_matrix_multiply(T *A, T *B, T* C, int width, int C_rows, int C_cols, gpc_id* myid)
+__global__ void zero_vector_float(float *vec, const int n)
 {
-  int row = blockIdx.y * blockDim.y + threadIdx.y;   
-  int col = blockIdx.x * blockDim.x + threadIdx.x;
-  //uint bad_SM = 0;
-  //uint bad_thrd = 23;
-  //int error = 0.0f;
-  
-  // check boundry conditions
-  if( row < C_rows && col < C_cols ){
-    // do the multiplication for one row and col
-    T value = 0;
-    for(int k = 0; k < width; k++){
-      value += A[row * width + k] * B[k * C_cols + col];
-    }
-    // store result
-    C[row * C_cols + col] = value;
-    myid[row * C_cols + col] = get_gpcid();
-    
-    // Inject Error
-    /*
-    if(myid[row * C_cols + col].sm_id == bad_SM)
-        if(myid[row * C_cols + col].t_idy == bad_thrd) //cannot assume all the cores in SM are faulty.
-            C[row * C_cols + col] = error;
-    */
+  unsigned int xIndex = blockDim.x * blockIdx.x + threadIdx.x;
+  if ( xIndex < n )
+    vec[xIndex]=0.0f;
+}
+
+// // Matrix-vector multiplication using CUDA
+// // Using shared memory and avoiding banking conflicts
+// xt::xarray<_Tp> matvecmul( xt::xarray<_Tp> matrix_W,
+//                         xt::xarray<_Tp> matrix_X)
+__global__ void MatMulKernel(float *out, float *in, float *a, const int matrixHeight, const int matrixWidth) {
+  // get variables for loop
+  // copy section of b into shared mem
+  // go through the threads vertically and sum them into a variable
+  // atomic add these variables to the corresponding c index
+
+  // looping is happening horizontally on the matrix
+  // BLOCK_WIDTH is again horizontal
+  // BLOCK_HEIGHT is going vertical
+  // n / BLOCK_WIDTH blocks horizontally
+  // m / BLOCK_HEIGHT block vertically
+
+  // get variables for loop
+  // variable for loop length: blockEltHeight
+  __shared__ int blockElt;
+  __shared__ int blockxInd;
+  __shared__ int blockyInd;
+  if (threadIdx.x == 0) {
+    if ((blockIdx.x + 1) * BLOCK_WIDTH <= matrixWidth)
+      blockElt = BLOCK_WIDTH;
+    else blockElt = matrixWidth % BLOCK_WIDTH;
+    blockxInd = blockIdx.x * BLOCK_WIDTH;
+    blockyInd = blockIdx.y * BLOCK_HEIGHT;
   }
+  
+  __syncthreads();
+  
+  // copy section of b into shared mem
+  // use the first BLOCK_WIDTH of thread
+  __shared__ float b[BLOCK_WIDTH];
+
+  if (threadIdx.x < blockElt) 
+    b[threadIdx.x] = in[blockxInd + threadIdx.x];
+  
+  __syncthreads();
+
+  // summing variable
+  float cSum = (float) 0;
+  int threadyInd = blockyInd + threadIdx.x;
+
+  // make sure we are inside the matrix verticallly
+  if (threadyInd < matrixHeight) {
+  
+    // go through the threads vertically and sum them into a variable
+    for (int i=0; i<blockElt; i++)
+      // A col index   : blockIdx.x * BLOCK_WIDTH + i : blockxInd + i
+      // A row index  : blockIdx.y * BLOCK_HEIGHT + threadIdx.x : blockyInd + threadIdx.x : threadyInd
+      // B index : b[i]
+
+      // cSum = B index * ( A col index * matrixHeight + A row index)
+      cSum += b[i] * a[(blockxInd + i) * (matrixHeight) + (threadyInd)];
+      //printf("csum = %f\n", cSum);
+    
+    // atomic add these variables to the corresponding c index
+    atomicAdd(out + threadyInd, cSum);
+  }
+  
 }
 
-template <class _Tp>
-xt::xarray<_Tp> matmul( xt::xarray<_Tp> matrix_X,
-                        xt::xarray<_Tp> matrix_Y)
-{
-  unsigned long X_rows = matrix_X.shape()[0];
-  unsigned long X_cols = matrix_X.shape()[1];
+float matVecMul (float * C, float * B, float * A, const int m, const int n)
+{  
   
-  unsigned long Y_rows = matrix_Y.shape()[0];
-  unsigned long Y_cols = matrix_Y.shape()[1];
+  int size_A = n * m * sizeof(float);
+  int size_B = n * sizeof(float);
+  int size_C = m * sizeof(float);
   
-  unsigned long Z_rows = X_rows;
-  unsigned long Z_cols = Y_cols;
+  // declare matrices for GPU and allocate memory
+  float *d_A, *d_B, *d_C;
   
-  unsigned long X_size = X_rows * X_cols;
-  unsigned long Y_size = Y_rows * Y_cols;
-  unsigned long Z_size = Z_rows * Z_cols;
-  
-  // host copies of X,Y,Z
-  _Tp *X = new _Tp[X_size];
-  _Tp *Y = new _Tp[Y_size]; 
-  _Tp *Z = new _Tp[Z_size];
-  gpc_id *myid = new gpc_id[Z_size];
-  
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&X, X_size*sizeof(_Tp));
-  cudaMallocManaged(&Y, Y_size*sizeof(_Tp));
-  cudaMallocManaged(&Z, Z_size*sizeof(_Tp));
-  cudaMallocManaged(&myid, Z_size*sizeof(gpc_id));
+  cudaMalloc((void**) &d_A, size_A);
+  cudaMalloc((void**) &d_B, size_B);
+  cudaMalloc((void**) &d_C, size_C);
 
-  
-  // Fill the matrix values from xtensor to C++ array
-  for (int i = 0; i < X_size; i++)
-  X[i] = matrix_X.flat(i);
-   
-  for (int i = 0; i < Y_size; i++)
-  Y[i] = matrix_Y.flat(i);
-    
-  // Matrix Multiplication on GPU
-  // Determine Grid/Block Size
-  // https://stackoverflow.com/questions/9985912/how-do-i-choose-grid-and-block-dimensions-for-cuda-kernels
-  unsigned long ROW_TILE_WIDTH, COL_TILE_WIDTH;
-  unsigned long DIM_COL_WIDTH, DIM_ROW_WIDTH;
-  
-  ROW_TILE_WIDTH = 64; //LAYER WIDTH
-  COL_TILE_WIDTH = 1; 
+  // copy elements from CPU to GPU
+  cudaMemcpy(d_A, A, size_A, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, B, size_B, cudaMemcpyHostToDevice);
 
-  DIM_COL_WIDTH  = 1;
-  DIM_ROW_WIDTH = 2;
-  
-  // Check if enough CUDA cores are allocated
-  assert (ROW_TILE_WIDTH*COL_TILE_WIDTH*DIM_COL_WIDTH*DIM_ROW_WIDTH >= LAYER_WIDTH && "Not enough CUDA cores allocated." );
-  
-  dim3 dim_grid(DIM_COL_WIDTH, DIM_ROW_WIDTH, 1);
-  dim3 dim_block(COL_TILE_WIDTH, ROW_TILE_WIDTH, 1);
+  //run mat-vec multiplication
+  // set up threading and blocking variables
+  cudaDeviceProp dp;
+  cudaGetDeviceProperties(&dp,0);
+  unsigned int max_threads_per_block = dp.maxThreadsPerBlock;
 
-  naive_matrix_multiply<_Tp><<<dim_grid, dim_block>>>(X, Y, Z, X_cols, Z_rows, Z_cols, myid);
+  int threads_perblockm = min(m, max_threads_per_block);
+  dim3 threadsPerBlockm(threads_perblockm);
+  int num_blocksm = (int)ceil((float)m/(float)threads_perblockm);
+  dim3 numBlocksm(num_blocksm);
 
-  // Wait for GPU to finish before accessing on host
+  int blockCols = (int) ceil(n / (double) BLOCK_WIDTH);
+  int blockRows = (int) ceil(m / (double) BLOCK_HEIGHT);
+  dim3 dimBlock(BLOCK_HEIGHT);
+  dim3 dimGrid(blockCols, blockRows);
+
+  int sharedMem = 3 * sizeof (int) + BLOCK_WIDTH * sizeof (float);
+
+  // execute kernels
+  zero_vector_float<<<numBlocksm, threadsPerBlockm>>>(d_C, m);
+  MatMulKernel<<<dimGrid, dimBlock, sharedMem>>>(d_C, d_B, d_A, m, n);
+
   cudaDeviceSynchronize();
   
-  // Convert product matrix to xtensor
-  xt::xarray<double>::shape_type matrix_Z_shape = {Z_rows, Z_cols};
-  xt::xarray<float> matrix_Z = xt::adapt(Z, Z_size, xt::no_ownership(), matrix_Z_shape);
-  
-  
-  // Log the output of myid
-  // https://stackoverflow.com/questions/25918057/how-to-set-a-fixed-width-with-cout
-  size_t headerWidths[5] = {std::string("T_IDX  ").size(),
-                            std::string("WRP_ID  ").size(),
-                            std::string("SM_ID  ").size(),
-                            std::string("GRID_ID  ").size(),
-                            std::string("CTA_IDX  ").size()
-                            };
-  // Redirecting output to a file
-  // https://stackoverflow.com/questions/10150468/how-to-redirect-cin-and-cout-to-files
-  // https://stackoverflow.com/questions/29464578/append-std-output-of-a-function-to-a-file
-  
-  const std::string cuda_log_file = "../cuda_logfiles/cuda_log-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + ".txt";
-  std::ofstream out(cuda_log_file, std::fstream::app);
-  std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
-  std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
+  // copy results back to CPU
+  cudaMemcpy(C, d_C, size_C, cudaMemcpyDeviceToHost);
 
-  std::cout << "T_IDX  T_IDY  T_IDZ  WRP_ID  SM_ID  GRID_ID  CTA_IDX  CTA_IDY  CTA_IDZ"<< std::endl;
-  for (int i = 0; i < Z_size; i++){
-      std::cout << std::left << std::setw(headerWidths[0]) << myid[i].t_idx;
-      std::cout << std::left << std::setw(headerWidths[0]) << myid[i].t_idy;
-      std::cout << std::left << std::setw(headerWidths[0]) << myid[i].t_idz;
-      std::cout << std::left << std::setw(headerWidths[1]) << myid[i].warp_id;
-      std::cout << std::left << std::setw(headerWidths[2]) << myid[i].sm_id;
-      std::cout << std::left << std::setw(headerWidths[3]) << myid[i].grid_id;
-      std::cout << std::left << std::setw(headerWidths[4]) << myid[i].cta_idx;
-      std::cout << std::left << std::setw(headerWidths[4]) << myid[i].cta_idy;
-      std::cout << std::left << std::setw(headerWidths[4]) << myid[i].cta_idz;
-      std::cout << std::endl;
-  }
-  std::cout << "***************************" << std::endl;
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
   
-  std::cout.rdbuf(coutbuf); //reset to standard output again
-  
-    
-  // Free memory
-  cudaFree(X);
-  cudaFree(Y);
-  cudaFree(Z);
-  cudaFree(myid);
-  
-  return matrix_Z;
-  
+  return 0;
 }
 
 
-int main(int argc, char *argv[])
+// int main(int argc, char *argv[])
+int main()
 {
- // load weights from npy files
+    int m = 512;
+    int n = 1024;
+  
+    // declare matrices for CPU and allocate memory
+    float *A = (float *) malloc (m * n * sizeof(float));
+    float *B = (float *) malloc (n * sizeof(float));
+    float *C = (float *) malloc (m * sizeof(float));
     
-    const std::string dense_weights_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_weights.npy";
-    const std::string dense_biases_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_biases.npy";
+    // randomly fill in elements of CPU matrices
+    createRandomMatrix(A, m * n, time(NULL));
+    createRandomMatrix(B, n, time(NULL));
     
-    const std::string dense_weights_1_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_1_weights.npy";
-    const std::string dense_biases_1_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_1_biases.npy";
+    matVecMul (C, B, A, m, n);
+    
+    free(A);
+    free(B);
+    free(C); 
+  
+    return 0;
+    
+    
+    
+    /*********************************************************************************/
+//  // load weights from npy files
+    
+//     const std::string dense_weights_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_weights.npy";
+//     const std::string dense_biases_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_biases.npy";
+    
+//     const std::string dense_weights_1_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_1_weights.npy";
+//     const std::string dense_biases_1_file = "../data/weights/mnist_dense-w" + std::to_string(LAYER_WIDTH) + "-" + std::to_string(MODEL_SEED) + "_dense_1_biases.npy";
  
-    xt::xarray<float> dense_weights = xt::load_npy<float>(dense_weights_file);
-    xt::xarray<float> dense_biases = xt::load_npy<float>(dense_biases_file);
-    dense_biases.reshape({-1, 1});
+//     xt::xarray<float> dense_weights = xt::load_npy<float>(dense_weights_file);
+//     xt::xarray<float> dense_biases = xt::load_npy<float>(dense_biases_file);
+//     dense_biases.reshape({-1, 1});
     
-    xt::xarray<float> dense_1_weights = xt::load_npy<float>(dense_weights_1_file);
-    xt::xarray<float> dense_1_biases = xt::load_npy<float>(dense_biases_1_file);
-    dense_1_biases.reshape({-1, 1});
+//     xt::xarray<float> dense_1_weights = xt::load_npy<float>(dense_weights_1_file);
+//     xt::xarray<float> dense_1_biases = xt::load_npy<float>(dense_biases_1_file);
+//     dense_1_biases.reshape({-1, 1});
 
 
-    // load mnist data
-    mnist_loader train("../dataset/train-images-idx3-ubyte",
-                     "../dataset/train-labels-idx1-ubyte", 60000);
-    mnist_loader test("../dataset/t10k-images-idx3-ubyte",
-                    "..//dataset/t10k-labels-idx1-ubyte", 10000);
+//     // load mnist data
+//     mnist_loader train("../dataset/train-images-idx3-ubyte",
+//                      "../dataset/train-labels-idx1-ubyte", 60000);
+//     mnist_loader test("../dataset/t10k-images-idx3-ubyte",
+//                     "..//dataset/t10k-labels-idx1-ubyte", 10000);
     
-    //combine test and train images
-    std::vector<std::vector<double>> all_images;
-    int all_labels[70000];
+//     //combine test and train images
+//     std::vector<std::vector<double>> all_images;
+//     int all_labels[70000];
     
-    for (int i = 0; i < 60000; i++)
-    {
-        all_images.push_back(train.images(i));
-        all_labels[i] = train.labels(i);
-    }
+//     for (int i = 0; i < 60000; i++)
+//     {
+//         all_images.push_back(train.images(i));
+//         all_labels[i] = train.labels(i);
+//     }
     
-    for (int i = 0; i < 10000; i++)
-    {
-        all_images.push_back(test.images(i));
-        all_labels[60000+i] = test.labels(i);
-    }
+//     for (int i = 0; i < 10000; i++)
+//     {
+//         all_images.push_back(test.images(i));
+//         all_labels[60000+i] = test.labels(i);
+//     }
 
 
-    /*check for the image <image_no> and display truth label*/
-    // https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c   
-    int image_no = std::stoi(argv[1]); //convert argument string to int
-    int label = all_labels[image_no];
-    std::cout << "IMAGE_NUMBER: " << image_no << std::endl;
-    std::cout << "TRUTH_LABEL:  " << label << std::endl;
+//     /*check for the image <image_no> and display truth label*/
+//     // https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c   
+//     int image_no = std::stoi(argv[1]); //convert argument string to int
+//     int label = all_labels[image_no];
+//     std::cout << "IMAGE_NUMBER: " << image_no << std::endl;
+//     std::cout << "TRUTH_LABEL:  " << label << std::endl;
     
-    // load the image <image_no> into vector and convert to xtensor<float32>
-     std::vector<double> image = all_images[image_no];
+//     // load the image <image_no> into vector and convert to xtensor<float32>
+//      std::vector<double> image = all_images[image_no];
         
-    // cast to float32 from double and reshape to single batch size
-    xt::xarray<float> input_image = xt::adapt(image);
-    input_image.reshape({-1, 1});
+//     // cast to float32 from double and reshape to single batch size
+//     xt::xarray<float> input_image = xt::adapt(image);
+//     input_image.reshape({-1, 1});
 
-    /******************LAYER 1******************/
-    // transpose weight matrix from (784, LAYER_WIDTH) -> (LAYER_WIDTH,784)
-    xt::xarray<float> tr_dense_weights = xt::transpose(dense_weights);
+//     /******************LAYER 1******************/
+//     // transpose weight matrix from (784, LAYER_WIDTH) -> (LAYER_WIDTH,784)
+//     xt::xarray<float> tr_dense_weights = xt::transpose(dense_weights);
     
-    // send through layer
-    xt::xarray<float> l1 = matmul<float>(tr_dense_weights, input_image);
+//     // send through layer
+//     xt::xarray<float> l1 = matVecMul<float>(tr_dense_weights, input_image);
     
-    // first layer bias
-    xt::xarray<float> b1 = l1 + dense_biases;
+//     // first layer bias
+//     xt::xarray<float> b1 = l1 + dense_biases;
     
-    // relu activation
-    xt::xarray<float> b1_relu = relu<float>(b1);
+//     // relu activation
+//     xt::xarray<float> b1_relu = relu<float>(b1);
 
-    /******************LAYER 2******************/
-    // transpose weight matrix
-    xt::xarray<float> tr_dense_1_weights = xt::transpose(dense_1_weights);
+//     /******************LAYER 2******************/
+//     // transpose weight matrix
+//     xt::xarray<float> tr_dense_1_weights = xt::transpose(dense_1_weights);
     
-    // send through layer
-    xt::xarray<float> l2 = matmul<float>(tr_dense_1_weights, b1_relu);
+//     // send through layer
+//     xt::xarray<float> l2 = matVecMul<float>(tr_dense_1_weights, b1_relu);
     
-    // second layer bias
-    xt::xarray<float> b2 = l2 + dense_1_biases;
+//     // second layer bias
+//     xt::xarray<float> b2 = l2 + dense_1_biases;
     
-    // softmax activation
-    xt::xarray<float> l3 = softmax(b2);
+//     // softmax activation
+//     xt::xarray<float> l3 = softmax(b2);
     
-    // argmax
-    std::cout << "PREDICTION:   " << xt::argmax(l3, 0)[0] << std::endl;
+//     // argmax
+//     std::cout << "PREDICTION:   " << xt::argmax(l3, 0)[0] << std::endl;
     
-  return 0; 
+//   return 0; 
 }
